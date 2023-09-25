5e05f37b3372eb597a87aab73e56c4b5c1efd76e
LightLM <maksim_olymp@mail.ru>
2023-09-25 03:28:47 +0300

    Рабочая версия Добавлено: 1. Загрузка инфы профилей из базы 2. Записывает действия сессии

--- flet_dev/flet_version.py
+++ flet_dev/flet_version.py
@@ -1,12 +1,15 @@
 import flet as ft
 import requests
+import json
 
 url = 'https://api.github.com/search/users?q='
 
 
 def main(page: ft.Page):
     def textbox_changed(e):
-        t.value = e.control.value
+        v = e.control.value
+        t.value = v
+        dict_a[action].append(v)
         t.update()
 
     def button_clicked(e):
@@ -21,17 +24,38 @@ def main(page: ft.Page):
                                   color=ft.colors.YELLOW
                                   )
             con.url = info['html_url']
-            dd.options = [ft.dropdown.Option(f'{i["name"]}') for i in requests.get(info['repos_url']).json()[:5]]
+            reps_nick = requests.get(info['repos_url']).json()[:5]
+            dd.options = [ft.dropdown.Option(f'{i["name"]}') for i in reps_nick]
             dd.hint_text = f'{len(requests.get(info["repos_url"]).json()[:5])} реп профиля под именем: {info["login"]}'
+            if not info["login"] in a:
+                a[info["login"]] = {'avatar_url': info['avatar_url'], 'reps': reps_nick, 'html_url': info['html_url']}
+                with open("data.json", "w") as refresh:
+                    json.dump(a, refresh)
+                with open("actions.json", "w") as refresh_actions:
+                    json.dump(dict_a, refresh_actions)
+                dd_2.options = [ft.dropdown.Option(i) for i in a]
         else:
             con.image_src = 'svadebnym_spetsialistam_kak_ispolzovat_vkontakte_dlya_prodvizheniya_biznesa.jpg'
             con.content = ft.Text(value=f'Такого профиля нет',
                                   color=ft.colors.YELLOW
                                   )
             con.url = ''
-        dd.update()
-        con.update()
-        print(res.text)
+        page.update()
+
+    def dropdown_changed(e):
+        t_dd.value = f"Ссылка на репу: https://github.com/{con.content.value.split()[-1]}/{dd.value}"
+        page.update()
+
+    def button_clicked_upload(e):
+        name = dd_2.value
+        con.image_src = a[name]['avatar_url']
+        con.url = a[name]['html_url']
+        con.content = ft.Text(value=f'Никнейм: {name}',
+                              color=ft.colors.YELLOW
+                              )
+        dd.options = [ft.dropdown.Option(f'{i["name"]}') for i in a[name]['reps']]
+        dd.hint_text = f'{len(a[name]["reps"])} реп профиля под именем: {name}'
+        page.update()
 
     con = ft.Container(
         content=ft.Text('Начнем поиск'),
@@ -45,27 +69,19 @@ def main(page: ft.Page):
     b = ft.IconButton(
         icon=ft.icons.PLAY_CIRCLE_FILL_OUTLINED, on_click=button_clicked, data=0
     )
+    bt = ft.IconButton(
+        icon=ft.icons.PLAY_CIRCLE_FILL_OUTLINED, on_click=button_clicked_upload, data=0
+    )
     t = ft.Text(
         width=300
     )
+    t_dd = ft.Text()
     tb = ft.TextField(
         label='Введи никнейм, который хочешь найти:',
         on_change=textbox_changed,
         width=300,
     )
-    page.add(ft.Row(
-        controls=[tb, b]
-    ))
-    page.add(ft.Column(
-        controls=[t, con]
-    ))
 
-    def dropdown_changed(e):
-        t_dd.value = f"Ссылка на репу: https://github.com/{con.content.value.split()[-1]}/{dd.value}"
-
-        page.update()
-
-    t_dd = ft.Text()
     dd = ft.Dropdown(
         on_change=dropdown_changed,
         hint_text='Сначала найди по никнейму',
@@ -74,8 +90,33 @@ def main(page: ft.Page):
         ],
         width=300,
     )
+
+    dd_2 = ft.Dropdown(
+        hint_text='Загрузка профиля (по нику)',
+        options=[
+            ft.dropdown.Option(i) for i in a
+        ],
+        width=300,
+    )
+
+    page.add(ft.Row(
+        controls=[tb, b]
+    ))
+    page.add(ft.Column(
+        controls=[t, con]
+    ))
     page.add(dd, t_dd)
+    page.add(ft.Row([dd_2, bt]))
 
 
 if __name__ == '__main__':
+    with open("data.json", "r") as fh:
+        a = json.load(fh)
+    with open("actions.json", "r") as dict_actions:
+        dict_a = json.load(dict_actions)
+    for i in dict_a:
+        print(i)
+        print(dict_a[i])
+    action = str(int(list(dict(dict_a).keys())[-1]) + 1)
+    dict_a[action] = []
     ft.app(target=main)
